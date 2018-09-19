bf0a48da411a754fb3510e28ba46ad9433df8e80
Marcus Lugg <marcuslugg@googlemail.com>
2018-09-19 19:32:27 +0100

    DOC: Add docstring to create_database script

--- scripts/create_database.sh
+++ scripts/create_database.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+# Create a new PostgreSQL database storage cluster, start up a server for it and create a database there.
+
 database=$1
 database_storage_area=$2
 user=$3
@@ -8,7 +10,7 @@ user=$3
 initdb -D $database_storage_area
 pg_ctl -D $database_storage_area start
 
-# Create database for user so future psql commands can be executed by them
+# Create user database so future psql commands can be executed by them
 createdb $user
 
 # Create the required database
