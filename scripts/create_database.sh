7fd17902f9365e58fa6e1be6da788ed0275d3777
Marcus Lugg <marcuslugg@googlemail.com>
2018-09-19 19:25:48 +0100

    FIX: Create database cluster before starting server
    The previous script would not run as a database cluster has to be
    created before a postgresql server is started on it.

--- scripts/create_database.sh
+++ scripts/create_database.sh
@@ -2,6 +2,14 @@
 
 database=$1
 database_storage_area=$2
+user=$3
 
+# Create database storage area and start server
+initdb -D $database_storage_area
 pg_ctl -D $database_storage_area start
+
+# Create database for user so future psql commands can be executed by them
+createdb $user
+
+# Create the required database
 createdb $database
