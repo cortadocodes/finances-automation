a163397489a0ac5c372cd8c9a449605271780719
Marcus Lugg <marcuslugg@googlemail.com>
2018-10-02 16:43:08 +0100

    REF: Pull statements into functions
    * Refactor into functions
    * Add check for overwriting database cluster

--- scripts/create_finances_database.sh
+++ scripts/create_finances_database.sh
@@ -6,15 +6,48 @@ database="finances"
 database_storage_area="data/database_cluster"
 user="Marcus1"
 
-# Create database storage area and start server
-initdb -D $database_storage_area
-pg_ctl -D $database_storage_area start
 
-# Create user database so future psql commands can be executed by them
-createdb $user
+create_or_overwrite_cluster() {
+    # If the cluster exists already, ask the user if they want to overwrite it
+    if [ ! -d $database_storage_area ]; then
+        initdb -D $database_storage_area
+    else
+        while true; do
+            read -p "Database cluster already exists: would you like to overwrite it? " choice
 
-# Create the required database
-createdb $database
+            case $choice in
+                [Yy]* )
+                    pg_ctl -D $database_storage_area stop
+                    rm -r $database_storage_area
+                    initdb -D $database_storage_area
+                    break;;
 
-# Disconnect from server
-pg_ctl -D $database_storage_area stop
+                [Nn]* )
+                    exit;;
+
+                * )
+                    echo "Please answer yes or no:";;
+
+            esac
+        done
+    fi
+}
+
+
+create_database() {
+    # Start database server
+    pg_ctl -D $database_storage_area start
+
+    # Create user database so future psql commands can be executed by them
+    createdb $user
+
+    # Create the required database
+    createdb $database
+
+    # Disconnect from server
+    pg_ctl -D $database_storage_area stop
+}
+
+
+create_or_overwrite_cluster
+create_database
