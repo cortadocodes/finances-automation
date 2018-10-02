9c662392d52aedb7cbfca4b2d3646991209cf285
Marcus Lugg <marcuslugg@googlemail.com>
2018-10-02 20:07:43 +0100

    CLN: Remove unnecessary code in creation script

--- scripts/create_finances_database.sh
+++ scripts/create_finances_database.sh
@@ -3,15 +3,13 @@
 # Create a new PostgreSQL database storage cluster, start up a server for it and create a database there.
 
 database="finances"
-database_storage_area="data/database_cluster"
+database_storage_area="../data/database_cluster"
 user="Marcus1"
 
 
-create_or_overwrite_cluster() {
+overwrite_cluster() {
     # If the cluster exists already, ask the user if they want to overwrite it
-    if [ ! -d $database_storage_area ]; then
-        initdb -D $database_storage_area
-    else
+    if [ -d $database_storage_area ]; then
         while true; do
             read -p "Database cluster already exists: would you like to overwrite it? " choice
 
@@ -19,7 +17,6 @@ create_or_overwrite_cluster() {
                 [Yy]* )
                     pg_ctl -D $database_storage_area stop
                     rm -r $database_storage_area
-                    initdb -D $database_storage_area
                     break;;
 
                 [Nn]* )
@@ -35,6 +32,9 @@ create_or_overwrite_cluster() {
 
 
 create_database() {
+    # Create database cluster
+    initdb -D $database_storage_area
+
     # Start database server
     pg_ctl -D $database_storage_area start
 
@@ -44,10 +44,10 @@ create_database() {
     # Create the required database
     createdb $database
 
-    # Disconnect from server
+    # Stop server
     pg_ctl -D $database_storage_area stop
 }
 
 
-create_or_overwrite_cluster
+overwrite_cluster
 create_database
