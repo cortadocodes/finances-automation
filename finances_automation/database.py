d9617d468002f9c11694e99f21ed61ef2242301c
Marcus Lugg <marcuslugg@googlemail.com>
2018-10-02 16:05:27 +0100

    DOC: Add docstrings to Database class

--- finances_automation/database.py
+++ finances_automation/database.py
@@ -2,6 +2,14 @@ import psycopg2
 
 
 class Database:
+    """
+    Create a PostgreSQL database wrapper object, allowing queries and commands to be passed in and data to be passed
+    out.
+
+    :param str name: pre-existing database name
+    :param psycopg2.extensions.connection connection: connection to database
+    :param psycopg2.extenstions.cursor cursor: cursor for executing SQL queries
+    """
     def __init__(self, name):
         self.check_types(name)
 
@@ -10,19 +18,38 @@ class Database:
         self.cursor = None
 
     def check_types(self, name):
+        """
+        Check initialisation inputs are of correct type.
+
+        :param any name: database name
+        """
         if not isinstance(name, str):
             raise TypeError('name must be a string.')
 
     def connect(self):
+        """
+        Connect to the database.
+        """
         self.connection = psycopg2.connect(dbname=self.name)
         self.cursor = self.connection.cursor()
 
     def disconnect(self):
+        """
+        Disconnect from the database.
+        """
         if self.connection is not None:
             self.cursor.close()
             self.connection.close()
 
     def execute_statement(self, statement, output_required=False):
+        """
+        Execute a SQL statement.
+
+        :param str statement: valid SQL statement, command or query
+        :param bool output_required: True if data is expected to be returned
+
+        :return list: output from database due to statement
+        """
         if not isinstance(statement, str):
             raise TypeError('statement must be a string.')
 
@@ -37,6 +64,11 @@ class Database:
         return output
 
     def describe(self):
+        """
+        Describe the database's structure (tables).
+
+        :return list: table names and metadata
+        """
         describe_statement = "SELECT * FROM information_schema.tables where table_schema = 'public'"
         tables = self.execute_statement(describe_statement, output_required=True)
         return tables
