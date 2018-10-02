726bcd074c5f2ccd9c9ba82ee77844b8bf7b36d0
Marcus Lugg <marcuslugg@googlemail.com>
2018-10-02 19:29:01 +0100

    TST: Include start/stop of psql server

--- finances_automation/tests/test_database.py
+++ finances_automation/tests/test_database.py
@@ -11,16 +11,25 @@ DATABASE_CLUSTER = os.path.join('..', 'data', 'database_cluster')
 
 def test_connect():
     db = Database(FINANCES_DATABASE, DATABASE_CLUSTER)
+    db.start()
     db.connect()
+
     assert isinstance(db.connection, psycopg2.extensions.connection)
     assert db.connection.closed == 0
     assert isinstance(db.cursor, psycopg2.extensions.cursor)
     assert db.cursor.closed is False
 
+    db.disconnect()
+    db.stop()
+
 
 def test_disconnect():
     db = Database(FINANCES_DATABASE, DATABASE_CLUSTER)
+    db.start()
     db.connect()
     db.disconnect()
+
     assert db.connection.closed != 0
     assert db.cursor.closed is True
+
+    db.stop()
