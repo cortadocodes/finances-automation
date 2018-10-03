bc527ed5edbec9d9544542d346969cffdfb7f515
Marcus Lugg <marcuslugg@googlemail.com>
2018-10-03 18:01:01 +0100

    REF: Convert script to just use bash arguments
    This is so it can be controlled from within python more easily.

--- scripts/create_finances_database.sh
+++ scripts/create_finances_database.sh
@@ -3,66 +3,46 @@
 # Create a new PostgreSQL database storage cluster, start up a server for it and create a database there.
 
 
-database="finances"
-database_storage_area="../data/database_cluster"
-user="Marcus1"
-overwrite="$1"
+database="$1"
+database_storage_area="$2"
+user="$3"
+overwrite="$4"
 
 
 check_overwrite() {
     # If the cluster exists already, ask the user if they want to overwrite it
-    if [ -d $database_storage_area ] ; then
-        if [ "$overwrite" == "" ] ; then
-            get_user_choice
-        else
-            case $overwrite in
-                [Yy]* )
-                    continue;;
-                [Nn]* )
-                    exit;;
-            esac
-        fi
-        overwrite_cluster
+    if [ -d "$database_storage_area" ] ; then
+        case "$overwrite" in
+            [Yy]* )
+                overwrite_cluster;;
+            [Nn]* )
+                exit;;
+        esac
     fi
 }
 
 
-get_user_choice() {
-    while true; do
-        read -p "Database cluster already exists: would you like to overwrite it? " overwrite
-            case $overwrite in
-                [Yy]* )
-                    break;;
-                [Nn]* )
-                    exit;;
-                * )
-                    echo "Please answer yes or no:";;
-        esac
-    done
-}
-
-
 overwrite_cluster() {
-    pg_ctl -D $database_storage_area stop
-    rm -r $database_storage_area
+    pg_ctl -D "$database_storage_area" stop
+    rm -r "$database_storage_area"
 }
 
 
 create_database() {
     # Create database cluster
-    initdb -D $database_storage_area
+    initdb -D "$database_storage_area"
 
     # Start database server
-    pg_ctl -D $database_storage_area start
+    pg_ctl -D "$database_storage_area" start
 
     # Create user database so future psql commands can be executed by them
-    createdb $user
+    createdb "$user"
 
     # Create the required database
-    createdb $database
+    createdb "$database"
 
     # Stop server
-    pg_ctl -D $database_storage_area stop
+    pg_ctl -D "$database_storage_area" stop
 }
 
 
