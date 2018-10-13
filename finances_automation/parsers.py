2a3a4bca8392a182312b0b95485d379f1de3b0c0
Marcus Lugg <marcuslugg@googlemail.com>
2018-10-13 17:46:46 +0100

    ENH: Remove unwanted characters; convert negatives
    * Remove '£' and ',' from monetary values
    * Convert bracket notation to negative value e.g. '(300)' to '-300'

--- finances_automation/parsers.py
+++ finances_automation/parsers.py
@@ -25,13 +25,22 @@ class Parser:
     def read(self, delimiter=',', header=0):
         self.data = pd.read_csv(self.file, delimiter=delimiter, header=header)
 
-    def clean(self):
+    def clean(self, monetary_columns):
         self.data.dropna(axis=0, how='all', inplace=True)
         self.data.dropna(axis=1, how='all', inplace=True)
         self.data.drop_duplicates(inplace=True)
+        self.remove_unwanted_characters(monetary_columns)
+        self.convert_negative_values(monetary_columns)
 
-    def parse_negative_values(self):
-        pass
+    def remove_unwanted_characters(self, monetary_columns):
+        for column in monetary_columns:
+            self.data[column] = self.data[column].str.replace('£', '').str.replace(',', '')
+
+    def convert_negative_values(self, monetary_columns):
+        negatives_values = r'\((\d*\.*\d*)\)'
+        replacement = r'-\1'
+        for column in monetary_columns:
+            self.data[column] = self.data[column].str.replace(negatives_values, replacement)
 
 
 DB_LOCATION = os.path.join('..', 'data', 'database_cluster')
@@ -40,4 +49,4 @@ STATEMENT_LOCATION = os.path.join('..', 'data', 'example_statement.csv')
 
 p = Parser('finances', 'Marcus1', DB_LOCATION, STATEMENT_LOCATION)
 p.read(header=3)
-p.clean()
+p.clean(monetary_columns=['Money in', 'Money Out', 'Balance'])
