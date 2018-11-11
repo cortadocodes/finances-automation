80bdc5ef1976638e4a66aa3dd61ff8e8d030bd55
Marcus Lugg <marcuslugg@googlemail.com>
2018-11-11 10:56:57 +0000

    FIX: Use absolute path for database location

--- finances_automation/scripts/parse_statement.py
+++ finances_automation/scripts/parse_statement.py
@@ -5,7 +5,8 @@ from finances_automation.parsers import Parser
 
 
 DB_NAME = 'finances'
-DB_LOCATION = os.path.join('..', 'data', 'database_cluster')
+PACKAGE_ROOT = os.path.abspath('..')
+DB_LOCATION = os.path.join(PACKAGE_ROOT, '..', 'data', 'database_cluster')
 DB_TABLE = 'transactions'
 USER = 'Marcus1'
 STATEMENT_LOCATION = sys.argv[1]
@@ -13,7 +14,13 @@ MONETARY_COLUMNS = ['money_in', 'money_out', 'balance']
 DATE_COLUMN = 'date'
 
 
-def parse_statement(db_name, db_location, db_table, user, statement_location, monetary_columns, date_column):
+def parse_statement(db_name,
+                    db_location,
+                    db_table,
+                    user,
+                    statement_location,
+                    monetary_columns,
+                    date_column):
     p = Parser(db_name, db_location, user, statement_location)
     p.read(header=3)
     p.clean(monetary_columns, date_column)
@@ -21,4 +28,12 @@ def parse_statement(db_name, db_location, db_table, user, statement_location, mo
 
 
 if __name__ == '__main__':
-    parse_statement(DB_NAME, DB_LOCATION, DB_TABLE, USER, STATEMENT_LOCATION, MONETARY_COLUMNS, DATE_COLUMN)
+    parse_statement(
+        DB_NAME,
+        DB_LOCATION,
+        DB_TABLE,
+        USER,
+        STATEMENT_LOCATION,
+        MONETARY_COLUMNS,
+        DATE_COLUMN
+    )
