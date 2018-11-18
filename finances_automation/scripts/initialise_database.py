514886e2c8d9e1cf62ee62eaa1467a59025f3e9a
Marcus Lugg <marcuslugg@googlemail.com>
2018-11-18 10:33:06 +0000

    CLN: Reorder and rename variables; pull out table columns

--- finances_automation/scripts/initialise_database.py
+++ finances_automation/scripts/initialise_database.py
@@ -3,36 +3,55 @@ import os
 from finances_automation.database import Database
 
 
-DATABASE_NAME = 'finances'
 PACKAGE_ROOT = os.path.abspath('..')
-DATABASE_CLUSTER= os.path.join(PACKAGE_ROOT, '..', 'data', 'database_cluster')
 USER = 'Marcus1'
+
+DB_NAME = 'finances'
+DB_CLUSTER = os.path.join(PACKAGE_ROOT, '..', 'data', 'database_cluster')
+
+TABLE_NAME = 'transactions'
+
 OVERWRITE = True
 
-CREATE_TRANSACTIONS_TABLE = """
-    CREATE TABLE transactions (
-         id serial PRIMARY KEY,
-         date DATE NOT NULL,
-         card VARCHAR,
-         description VARCHAR,
-         money_in DECIMAL,
-         money_out DECIMAL,
-         balance DECIMAL NOT NULL,
-         category_code INT,
-         category VARCHAR
-    );
-"""
+TABLE_HEADERS = [
+    'id',
+    'date',
+    'card',
+    'description',
+    'money_in',
+    'money_out',
+    'balance',
+    'category_code',
+    'category'
+]
+
+CREATE_TRANSACTIONS_TABLE = (
+    """
+        CREATE TABLE {} (
+             {} serial PRIMARY KEY,
+             {} DATE NOT NULL,
+             {} VARCHAR,
+             {} VARCHAR,
+             {} DECIMAL,
+             {} DECIMAL,
+             {} DECIMAL NOT NULL,
+             {} INT,
+             {} VARCHAR
+        );
+    """
+    .format(DB_NAME, *TABLE_HEADERS)
+)
 
 
-def initialise_finances_database():
+def initialise_finances_database(db_name, db_cluster, user, table_creation_query):
     """ Create and initialise finances database with empty transactions table.
     """
-    database = Database(DATABASE_NAME, DATABASE_CLUSTER, USER)
+    database = Database(db_name, db_cluster, user)
     database.create(overwrite=True)
     database.start()
-    database.execute_statement(CREATE_TRANSACTIONS_TABLE)
+    database.execute_statement(table_creation_query)
     database.stop()
 
 
 if __name__ == '__main__':
-    initialise_finances_database()
+    initialise_finances_database(DB_NAME, DB_CLUSTER, USER, CREATE_TRANSACTIONS_TABLE)
