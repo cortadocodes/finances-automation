bd7bf2e1fe40159ab0697ab8a045e3b0af268212
Marcus Lugg <marcuslugg@googlemail.com>
2018-11-18 16:04:29 +0000

    REF: Get date variables; update usage of functions/class

--- finances_automation/scripts/categorise_transactions.py
+++ finances_automation/scripts/categorise_transactions.py
@@ -1,18 +1,41 @@
+import sys
+
 from finances_automation.categorise import Categoriser
 from finances_automation.scripts import configuration as conf
 
 
+START_DATE = sys.argv[1]
+END_DATE = sys.argv[2]
+
+
 def categorise_transactions(db_name,
                             db_location,
-                            user,
+                            db_user,
+                            table_name,
+                            table_headers,
+                            date_column,
+                            start_date,
+                            end_date,
                             income_categories,
                             expense_categories,
-                            table_name,
-                            table_headers):
-    categoriser = Categoriser(db_name, db_location, user, income_categories, expense_categories)
-    categoriser.load_from_database(table_name, table_headers)
+                            category_columns):
+
+    categoriser = Categoriser(
+        db_name,
+        db_location,
+        db_user,
+        table_name,
+        table_headers,
+        income_categories,
+        expense_categories,
+        category_columns,
+        date_column,
+        start_date,
+        end_date
+    )
+    categoriser.load_from_database()
     categoriser.select_categories()
-    categoriser.store_in_database(table_name)
+    categoriser.store_in_database()
 
 
 if __name__ == '__main__':
@@ -20,8 +43,12 @@ if __name__ == '__main__':
         conf.DB_NAME,
         conf.DB_CLUSTER,
         conf.USER,
+        conf.TABLE_NAME,
+        conf.TABLE_HEADERS,
         conf.INCOME_CATEGORIES,
         conf.EXPENSE_CATEGORIES,
-        conf.TABLE_NAME,
-        conf.TABLE_HEADERS
+        conf.CATEGORY_COLUMNS,
+        conf.DATE_COLUMN,
+        START_DATE,
+        END_DATE
     )
