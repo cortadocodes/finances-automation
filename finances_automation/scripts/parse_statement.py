8ee6ce4a8303dc669bd8e2227cd9bcb46e22acb5
Marcus Lugg <marcuslugg@googlemail.com>
2018-11-18 10:27:12 +0000

    CLN: Reorder and rename variables

--- finances_automation/scripts/parse_statement.py
+++ finances_automation/scripts/parse_statement.py
@@ -4,12 +4,16 @@ import sys
 from finances_automation.parsers import Parser
 
 
-DB_NAME = 'finances'
-PACKAGE_ROOT = os.path.abspath('..')
-DB_LOCATION = os.path.join(PACKAGE_ROOT, '..', 'data', 'database_cluster')
-DB_TABLE = 'transactions'
-USER = 'Marcus1'
 STATEMENT_LOCATION = sys.argv[1]
+
+PACKAGE_ROOT = os.path.abspath('..')
+USER = 'Marcus1'
+
+DB_NAME = 'finances'
+DB_LOCATION = os.path.join(PACKAGE_ROOT, '..', 'data', 'database_cluster')
+
+TABLE_NAME = 'transactions'
+
 MONETARY_COLUMNS = ['money_in', 'money_out', 'balance']
 DATE_COLUMN = 'date'
 
@@ -31,7 +35,7 @@ if __name__ == '__main__':
     parse_statement(
         DB_NAME,
         DB_LOCATION,
-        DB_TABLE,
+        TABLE_NAME,
         USER,
         STATEMENT_LOCATION,
         MONETARY_COLUMNS,
