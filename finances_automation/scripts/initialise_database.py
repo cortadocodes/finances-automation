3b23b8526faa72e9716ea1529926f9f7e374fb5d
Marcus Lugg <marcuslugg@googlemail.com>
2018-11-18 13:38:24 +0000

    FIX: Enable toggling of database overwrite

--- finances_automation/scripts/initialise_database.py
+++ finances_automation/scripts/initialise_database.py
@@ -2,7 +2,7 @@ from finances_automation.database import Database
 from finances_automation.scripts import configuration as conf
 
 
-OVERWRITE = True
+REQUIRE_OVERWRITE = False
 
 CREATE_TABLE = (
     """CREATE TABLE {} ({});"""
@@ -12,11 +12,11 @@ CREATE_TABLE = (
 )
 
 
-def initialise_database(db_name, db_cluster, user, table_creation_query):
+def initialise_database(db_name, db_cluster, user, table_creation_query, require_overwrite):
     """ Create and initialise a database with an empty table.
     """
     database = Database(db_name, db_cluster, user)
-    database.create(overwrite=True)
+    database.create(overwrite=require_overwrite)
     database.start()
     database.execute_statement(table_creation_query)
     database.stop()
@@ -27,5 +27,6 @@ if __name__ == '__main__':
         conf.DB_NAME,
         conf.DB_CLUSTER,
         conf.USER,
-        CREATE_TABLE
+        CREATE_TABLE,
+        REQUIRE_OVERWRITE
     )
