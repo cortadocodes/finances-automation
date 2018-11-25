48391fc983694936c409ffa986f57a1c70db3510
Marcus Lugg <marcuslugg@googlemail.com>
2018-11-25 18:26:25 +0000

    FIX: Remove old arguments from Database

--- finances_automation/parsers.py
+++ finances_automation/parsers.py
@@ -24,7 +24,7 @@ class BaseParser:
         """
         self.check_types(file)
 
-        self.db = Database(conf.DB_NAME, conf.DB_CLUSTER, conf.USER)
+        self.db = Database()
         self.file = file
         self.data = None
 
