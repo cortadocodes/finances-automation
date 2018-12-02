ec19a37a9df2f1c77aa139a286bd82386b6debf4
Marcus Lugg <marcuslugg@googlemail.com>
2018-12-02 18:05:13 +0000

    REF: Rename Table attribute

--- finances_automation/table.py
+++ finances_automation/table.py
@@ -1,10 +1,10 @@
 class Table:
 
-    def __init__(self, name, column_names, monetary_columns, date_column, date_format,
-                  category_columns):
+    def __init__(self, name, schema, monetary_columns, date_column, date_format,
+                 category_columns):
 
         self.name = name
-        self.column_names = column_names
+        self.schema = schema
         self.monetary_columns = monetary_columns
         self.date_column = date_column
         self.date_format = date_format
@@ -17,7 +17,7 @@ class Table:
         if not isinstance(name, str):
             raise TypeError('name should be a string.')
         if not isinstance(column_names, list):
-            raise TypeError('column_names should be a list of strings.')
+            raise TypeError('schema should be a list of strings.')
         if not isinstance(monetary_columns, str):
             raise TypeError('monetary_columns should be a list of strings.')
         if not isinstance(date_column, str):
