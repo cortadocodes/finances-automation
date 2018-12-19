b3f858a924433c808c0031168b647336d8018174
Marcus Lugg <marcuslugg@googlemail.com>
2018-12-19 21:38:53 +0000

    FIX: Fix Table import and unpacking of table data

--- finances_automation/operations/parse.py
+++ finances_automation/operations/parse.py
@@ -5,7 +5,7 @@ import pandas as pd
 
 from finances_automation import configuration as conf
 from finances_automation.entities.database import Database
-from finances_automation.table import Table
+from finances_automation.entities.table import Table
 
 
 class BaseParser:
