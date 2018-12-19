af24a57854dc7c764789eb21f810ac97657c0435
Marcus Lugg <marcuslugg@googlemail.com>
2018-12-19 21:49:35 +0000

    FIX: Get configuration attribute properly

--- finances_automation/api/categorise_transactions.py
+++ finances_automation/api/categorise_transactions.py
@@ -5,21 +5,22 @@ from finances_automation import configuration as conf
 from finances_automation.entities.table import Table
 
 
-table_name = sys.argv[1]
-for attr in conf.__dict__:
-    if hasattr(conf, table_name.upper()):
-        TABLE = Table(**conf.table_name)
-    else:
-        raise ValueError('No such table_name.')
-
+TABLE_NAME = sys.argv[1].upper()
 START_DATE = sys.argv[2]
 END_DATE = sys.argv[3]
+
+
+if hasattr(conf, TABLE_NAME):
+    TABLE_CONF = getattr(conf, TABLE_NAME)
+    TABLE = Table(**TABLE_CONF)
+else:
+    raise ValueError('No such TABLE_NAME.')
 
 
 def categorise_transactions():
     """ Load transactions from the database, categorise them, and update the database with these categories.
     """
-    categoriser = Categoriser(START_DATE, END_DATE, TABLE)
+    categoriser = Categoriser(TABLE, START_DATE, END_DATE)
     categoriser.load_from_database()
     categoriser.select_categories()
     categoriser.store_in_database()
