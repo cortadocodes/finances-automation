9ccce541f52e553d492669fce56c01ea6873e477
Marcus Lugg <marcuslugg@googlemail.com>
2019-02-07 20:49:08 +0000

    TST: Add test for getting Tables from configuration file

--- finances_automation/tests/test_entities/test_table.py
+++ finances_automation/tests/test_entities/test_table.py
@@ -1,9 +1,11 @@
 import pytest
 
+from finances_automation import configuration as conf
 from finances_automation.entities.table import Table
 
 
 class TestTable:
+
     def test_valid_table_instantiation(self):
         """ Test Table can be instantiated correctly without error.
 
@@ -31,7 +33,24 @@ class TestTable:
         """ Test Table's validator raises the correct errors when instantiated with incorrect arguments.
 
         :raise AssertionError:
-        :return:
+        :return None:
         """
         with pytest.raises(TypeError):
             Table(name, type, schema, monetary_columns, date_columns, date_format, category_columns)
+
+    @pytest.mark.parametrize('table_name', [name for name in conf.table_names])
+    def test_get_table(self, table_name):
+        """ Ensure a Table can be constructed faithfully from each of the configurations in the configuration file.
+
+        :raise AssertionError:
+        :return None:
+        """
+        table = Table.get_table(table_name)
+
+        expected_config = conf.table_configurations[table_name]
+
+        for attribute in table.__dict__:
+            if attribute == 'data':
+                continue
+
+            assert getattr(table, attribute) == expected_config[attribute]
