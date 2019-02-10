565468786d5c43dc9a7514f797326e3c15f33dd7
Marcus Lugg <marcuslugg@googlemail.com>
2019-02-10 09:46:20 +0000

    ENH: Use dictionary to specify types in Table validator

--- finances_automation/validation/table.py
+++ finances_automation/validation/table.py
@@ -5,20 +5,31 @@ def table_validator(func):
 
         :raise TypeError: if any of the parameters are of the wrong type
         """
+        types = {
+            'name': str,
+            'type': str,
+            'schema': dict,
+            'monetary_columns': list,
+            'date_columns': list,
+            'date_format': str,
+            'category_columns': list
+        }
+        error_message = '{} parameter should be of type {}; received {}.'
+
         if not isinstance(name, str):
-            raise TypeError('name should be a string.')
+            raise TypeError(error_message.format('name', types['name'], name))
         if not isinstance(type, str):
-            raise TypeError('type should be a string')
+            raise TypeError(error_message.format('type', types['type'], type))
         if not isinstance(schema, dict):
-            raise TypeError('schema should be a dictionary.')
+            raise TypeError(error_message.format('schema', types['schema'], schema))
         if monetary_columns and not isinstance(monetary_columns, list):
-            raise TypeError('monetary_columns should be a list of strings.')
+            raise TypeError(error_message.format('monetary_columns', types['monetary_columns'], monetary_columns))
         if date_columns and not isinstance(date_columns, list):
-            raise TypeError('date_columns should be a list of strings.')
+            raise TypeError(error_message.format('date_columns', types['date_columns'], date_columns))
         if date_format and not isinstance(date_format, str):
-            raise TypeError('date_format should be a string.')
+            raise TypeError(error_message.format('date_format', types['date_format'], date_format))
         if category_columns and not isinstance(category_columns, list):
-            raise TypeError('category_columns should be a list of strings.')
+            raise TypeError(error_message.format('category_columns', types['category_columns'], category_columns))
 
         return func(self, name, type, schema, monetary_columns, date_columns, date_format, category_columns)
 
