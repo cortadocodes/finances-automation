b6e2abcedb0dcc211e591163339f446fc11dd9cd
Marcus Lugg <marcuslugg@googlemail.com>
2019-02-10 15:53:43 +0000

    ENH: Make Table validator DRYer

--- finances_automation/validation/table.py
+++ finances_automation/validation/table.py
@@ -1,42 +1,29 @@
 def table_validator(func):
-    def table_validator(self, name, type_, schema, monetary_columns=None, date_columns=None, date_format=None,
+    def table_validator(instance, name, type_, schema, monetary_columns=None, date_columns=None, date_format=None,
                         category_columns=None):
         """ Check if Table initialisation parameters are of the correct type.
 
         :raise TypeError: if any of the parameters are of the wrong type
         """
-        types = {
-            'name': str,
-            'type_': str,
-            'schema': dict,
-            'monetary_columns': list,
-            'date_columns': list,
-            'date_format': str,
-            'category_columns': list
+        attribute_types = {
+            'name': [str],
+            'type_': [str],
+            'schema': [dict],
+            'monetary_columns': [list, type(None)],
+            'date_columns': [list, type(None)],
+            'date_format': [str, type(None)],
+            'category_columns': [list, type(None)]
         }
+
         error_message = '{} parameter should be of type {}; received {}.'
 
-        if not isinstance(name, types['name']):
-            raise TypeError(error_message.format('name', types['name'], name))
+        for attribute, allowed_types in attribute_types.items():
+            parameter = locals()[attribute]
 
-        if not isinstance(type_, types['type_']):
-            raise TypeError(error_message.format('type_', types['type_'], type_))
+            if any(isinstance(parameter, allowed_type) for allowed_type in allowed_types):
+                continue
+            raise TypeError(error_message.format(attribute, ' or '.join(allowed_types), parameter))
 
-        if not isinstance(schema, types['schema']):
-            raise TypeError(error_message.format('schema', types['schema'], schema))
-
-        if monetary_columns and not isinstance(monetary_columns, types['monetary_columns']):
-            raise TypeError(error_message.format('monetary_columns', types['monetary_columns'], monetary_columns))
-
-        if date_columns and not isinstance(date_columns, types['date_columns']):
-            raise TypeError(error_message.format('date_columns', types['date_columns'], date_columns))
-
-        if date_format and not isinstance(date_format, types['date_format']):
-            raise TypeError(error_message.format('date_format', types['date_format'], date_format))
-
-        if category_columns and not isinstance(category_columns, types['category_columns']):
-            raise TypeError(error_message.format('category_columns', types['category_columns'], category_columns))
-
-        return func(self, name, type_, schema, monetary_columns, date_columns, date_format, category_columns)
+        return func(instance, name, type_, schema, monetary_columns, date_columns, date_format, category_columns)
 
     return table_validator
