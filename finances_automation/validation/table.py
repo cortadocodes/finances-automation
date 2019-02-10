b94cda095baa852f5b9b5d3814c54a18c4aecbe2
Marcus Lugg <marcuslugg@googlemail.com>
2019-02-10 09:55:55 +0000

    ENH: Fully utilise types dictionary in Table validation

--- finances_automation/validation/table.py
+++ finances_automation/validation/table.py
@@ -16,19 +16,25 @@ def table_validator(func):
         }
         error_message = '{} parameter should be of type {}; received {}.'
 
-        if not isinstance(name, str):
+        if not isinstance(name, types['name']):
             raise TypeError(error_message.format('name', types['name'], name))
-        if not isinstance(type_, str):
+
+        if not isinstance(type_, types['type_']):
             raise TypeError(error_message.format('type_', types['type_'], type_))
-        if not isinstance(schema, dict):
+
+        if not isinstance(schema, types['schema']):
             raise TypeError(error_message.format('schema', types['schema'], schema))
-        if monetary_columns and not isinstance(monetary_columns, list):
+
+        if monetary_columns and not isinstance(monetary_columns, types['monetary_columns']):
             raise TypeError(error_message.format('monetary_columns', types['monetary_columns'], monetary_columns))
-        if date_columns and not isinstance(date_columns, list):
+
+        if date_columns and not isinstance(date_columns, types['date_columns']):
             raise TypeError(error_message.format('date_columns', types['date_columns'], date_columns))
-        if date_format and not isinstance(date_format, str):
+
+        if date_format and not isinstance(date_format, types['date_format']):
             raise TypeError(error_message.format('date_format', types['date_format'], date_format))
-        if category_columns and not isinstance(category_columns, list):
+
+        if category_columns and not isinstance(category_columns, types['category_columns']):
             raise TypeError(error_message.format('category_columns', types['category_columns'], category_columns))
 
         return func(self, name, type_, schema, monetary_columns, date_columns, date_format, category_columns)
