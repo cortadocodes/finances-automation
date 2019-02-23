27a445e87f10e05537452133c484f803a1602aac
Marcus Lugg <marcuslugg@googlemail.com>
2019-02-23 16:12:03 +0000

    REF: Clean up analyse API command

--- finances_automation/api/analyse_transactions.py
+++ finances_automation/api/analyse_transactions.py
@@ -6,36 +6,31 @@ from finances_automation.entities.table import Table
 from finances_automation.operations.analyse import Analyser
 
 
-EXCLUDED_FROM_STORAGE = Analyser.analyses_excluded_from_storage
 OUTPUT_PATH = os.path.join(conf.package_root, 'data')
 
 
-def analyse_transactions(analysis_type, tables_to_analyse, table_to_store, start_date, end_date):
+def analyse_transactions(analysis_type, table_to_analyse, start_date, end_date, table_to_store=None):
     """ Analyse transactions stored in a given database table between the given dates.
 
     :param str analysis_type:
-    :param list(Table) tables_to_analyse:
+    :param list(Table) table_to_analyse:
     :param Table table_to_store:
     :param str start_date:
     :param str end_date:
     """
-    analyser = Analyser(analysis_type, tables_to_analyse, table_to_store, start_date, end_date)
+    analyser = Analyser(analysis_type, table_to_analyse, start_date, end_date, table_to_store)
     analyser.analyse()
-    analyser.export_analysis(OUTPUT_PATH)
+    analyser.export(OUTPUT_PATH)
 
 
 if __name__ == '__main__':
     analysis_type = sys.argv[1].lower()
-    tables_to_analyse = [Table.get_from_config(table_name) for table_name in sys.argv[2].split(';')]
+    table_to_analyse = Table.get_from_config(sys.argv[2].lower())
     start_date = sys.argv[3]
     end_date = sys.argv[4]
+    table_to_store = sys.argv[5]
 
-    if len(tables_to_analyse) == 1:
-        tables_to_analyse = tables_to_analyse[0]
-
-    if analysis_type in EXCLUDED_FROM_STORAGE:
-        table_to_store = None
-    else:
+    if table_to_store:
         table_to_store = Table.get_from_config(analysis_type)
 
-    analyse_transactions(analysis_type, tables_to_analyse, table_to_store, start_date, end_date)
+    analyse_transactions(analysis_type, table_to_analyse, start_date, end_date, table_to_store)
