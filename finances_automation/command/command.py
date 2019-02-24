c3e67ac66d18bfd74d48f133901223899802834d
Marcus Lugg <marcuslugg@googlemail.com>
2019-02-24 13:36:35 +0000

    ENH: Give allowed arguments to CLI arguments

--- finances_automation/command/command.py
+++ finances_automation/command/command.py
@@ -1,32 +1,56 @@
 import argparse
 
+from finances_automation import configuration as conf
+from finances_automation.operations.analyses import get_available_analyses
+
 
 parser = argparse.ArgumentParser(prog='finances-automation', description='Automate finance analysis.')
 subparsers = parser.add_subparsers(title='Subcommands')
 
 parse_statement = subparsers.add_parser('parse', help='Parse a UTF-8 csv financial statement.')
 parse_statement.add_argument('file', help='Path of file to parse.')
-parse_statement.add_argument('table_name', help='Name of table to store parsed statement in.')
+parse_statement.add_argument(
+    'table_name',
+    choices=conf.transaction_table_names,
+    help='Name of table to store parsed statement in.'
+)
 
 categorise = subparsers.add_parser('categorise', help='Categorise transactions from a transactions table.')
-categorise.add_argument('table_name', help='Name of table containing transactions to categorise.')
+categorise.add_argument(
+    'table_name',
+    choices=conf.transaction_table_names,
+    help='Name of table containing transactions to categorise.'
+)
 categorise.add_argument('start_date', help='Start the categorisation from this date (inclusive).')
 categorise.add_argument('end_date', help='End the categorisation at this date (inclusive).')
 
 analyse = subparsers.add_parser('analyse', help='Analyse transactions from a transactions table.')
-analyse.add_argument('analysis_type', help='Name of analysis to carry out.')
-analyse.add_argument('table_to_analyse', help='Name of table containing transactions to analyse.')
+analyse.add_argument(
+    'analysis_type',
+    choices=get_available_analyses().keys(),
+    help='Name of analysis to carry out.'
+)
+analyse.add_argument(
+    'table_to_analyse',
+    choices=conf.analysis_table_names,
+    help='Name of table containing transactions to analyse.'
+)
 analyse.add_argument('start_date', help='Start the analysis from this date (inclusive).')
 analyse.add_argument('end_date', help='End the analysis at this date (inclusive).')
 analyse.add_argument('-s', '--table_to_store', help='Name of table to store the analysis in.')
 analyse.add_argument('-e', '--export', action='store_true', help='Export the analysis to the default directory.')
 
 view_latest = subparsers.add_parser('view_latest', help='View the latest n transactions from a table.')
-view_latest.add_argument('table_name', help='Name of table to view.')
+view_latest.add_argument(
+    'table_name',
+    choices=conf.table_names,
+    help='Name of table to view.'
+)
 view_latest.add_argument('-n', type=int, default=50, help='Number of transactions to view.')
 view_latest.add_argument('-c', '--columns', default=['*'], help='List of columns to view.')
 
 cli_args = parser.parse_args()
+
 
 # Only import command functions if functionality is requested; this keeps the CLI's response quick for help requests.
 from finances_automation import command
