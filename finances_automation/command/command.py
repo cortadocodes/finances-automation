3e24f6eafdf0a20875c1b6fb368452c45fc8504f
Marcus Lugg <marcuslugg@googlemail.com>
2019-02-24 16:04:27 +0000

    ENH: Use new location for CLI commands

--- finances_automation/command/command.py
+++ finances_automation/command/command.py
@@ -2,7 +2,6 @@ import argparse
 
 from finances_automation.command import command_parsers
 
-
 main_parser = argparse.ArgumentParser(prog='finances-automation', description='Automate your finances analysis.')
 subparsers = main_parser.add_subparsers(title='Subcommands')
 
@@ -15,13 +14,13 @@ cli_args = main_parser.parse_args()
 
 
 # Only import command functions if functionality is requested; this keeps the CLI's response quick for help requests.
-from finances_automation import command
+from finances_automation.command import commands
 
-main_parser.set_defaults(func=command.get_overview)
-parse_statement.set_defaults(func=command.parse_statement)
-categorise.set_defaults(func=command.categorise_transactions)
-analyse.set_defaults(func=command.analyse_transactions)
-view_latest.set_defaults(func=command.view_latest)
+main_parser.set_defaults(func=commands.get_overview)
+parse_statement.set_defaults(func=commands.parse_statement)
+categorise.set_defaults(func=commands.categorise_transactions)
+analyse.set_defaults(func=commands.analyse_transactions)
+view_latest.set_defaults(func=commands.view_latest)
 
 # Get the arguments again to allow access to the newly-imported functions.
 cli_args = main_parser.parse_args()
