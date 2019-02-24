022d0660d73e9cea79453f142bd3f1f9e97d0012
Marcus Lugg <marcuslugg@googlemail.com>
2019-02-24 13:29:28 +0000

    FIX: Update command.__init__.py

--- finances_automation/command/__init__.py
+++ finances_automation/command/__init__.py
@@ -2,4 +2,4 @@ from .analyse_transactions import analyse_transactions
 from .categorise_transactions import categorise_transactions
 from .get_overview import get_overview
 from .parse_statement import parse_statement
-from .view_table import view_latest
+from .view_latest import view_latest
