a5be51aaf4125431fb2b491edd30e2847078ea73
Marcus Lugg <marcuslugg@googlemail.com>
2019-03-22 14:59:40 +0000

    FIX: Ensure Database validator args in order

--- finances_automation/validation/database.py
+++ finances_automation/validation/database.py
@@ -2,18 +2,18 @@ from finances_automation.validation.base import validate_variables
 
 
 def database_validator(func):
-    def database_validator(instance, host, port, name, cluster, user, password=None, log_location=None):
+    def database_validator(instance, host, name, cluster, user, password='', port=5432, log_location=None):
         """ Check if Database initialisation parameters are of correct type.
 
         :raise TypeError: if any of the parameters are of the wrong type
         """
         allowed_parameter_types = {
             'host': [str],
-            'port': [str],
             'name': [str],
             'cluster': [str],
             'user': [str],
-            'password': [str, type(None)],
+            'password': [str],
+            'port': [int],
             'log_location': [str, type(None)]
         }
 
@@ -21,6 +21,6 @@ def database_validator(func):
         parameters = {parameter_name: locals_[parameter_name] for parameter_name in allowed_parameter_types}
         validate_variables(parameters, allowed_parameter_types)
 
-        return func(instance, host, port, name, cluster, user, password, log_location)
+        return func(instance, host, name, cluster, user, password, port, log_location)
 
     return database_validator
