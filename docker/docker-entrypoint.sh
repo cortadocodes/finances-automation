88ac8d38dfaf279a1160df15e951a8bec564e62a
Marcus Lugg <marcuslugg@googlemail.com>
2019-04-18 14:32:24 +0100

    ENH: Add docker entrypoint for db initialisation

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 set -e
 
+
+APPLICATION_ROOT=/usr/src/finances-automation/finances_automation
 TEST_REPORTS_DIRECTORY=/test-reports
 
-cd /usr/src/app/
 
 if [ "$1" == "app" ]; then
     finances-automation
 
 elif [ "$1" == "test" ]; then
-    pytest finances_automation/tests/ \
+    pytest $APPLICATION_ROOT/tests/ \
     --junit-xml="$TEST_REPORTS_DIRECTORY"/junit.xml \
-    --cov=finances_automation \
+    --cov="$APPLICATION_ROOT" \
     --cov-report html:"$TEST_REPORTS_DIRECTORY"/coverage_html
+
+elif [ "$1" == "initialise" ]; then
+    python3 "$APPLICATION_ROOT"/scripts/initialise_database.py
 
 else
     exec "$@"
